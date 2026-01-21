7a7c6dfbf4312c12bb2317457f0030c81f694cd1
skyw0rld <20lkhashan@uobschool.org.uk>
2026-01-21 12:49:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
 from pygame import *
+## https://uobschool.sharepoint.com/:p:/r/sites/ComputerScienceStudentResources/_layouts/15/Doc.aspx?sourcedoc=%7B218459FF-B59C-47CB-829B-BDFC78ADD6C4%7D&file=Space%20invaders.pptx&action=edit&mobileredirect=true
+
 
 init()
 
-invaderList = []
+width = 800
+height = 600
+iOFFSET = 40
+
+
+c = time.Clock()
 FPS = 60
 
+screen = display.set_mode((width,height))
 
-class Invader:
-    def __init__(self, x, y):
-        SIZE = 30
-        self.img = transform.scale(image.load("spaceinvader.png"), (SIZE,SIZE))
-        self.rect = Rect(x, y, self.img.get_width(), self.img.get_height())
-        self.dx = 1
-        self.dy = 0
-    def draw(self, screen):
-        screen.blit(self.img, self.rect)
-    def update(self):
-        global width, height
-        if self.rect.right + self.dx >= width or self.rect.left + self.dx <= 0:
-            self.dy = 20
-            self.dx = -self.dx
-        self.rect.x += self.dx
-        self.rect.y += self.dy
-        self.dy = 0
-       
+invaderList = []
+
+class Invader: # defines invader
+	def __init__(self, x, y): ## constructor (must be defined as __init__)
+		SIZE = 40
+		self.img = image.load("thumbnail_invader.png") ##loads the picture of invader
+		self.img = transform.scale(self.img, (SIZE, SIZE)) ## scales it to a said size (e.g 40, 40)
+		self.dx = 1 #direction
+		self.rect = Rect(x,y, ## labels x,y
+		                 self.img.get_width(), ##gets the width of the object
+		                 self.img.get_height() ##gets the height of the object
+		                 ) 
+		           
+	def draw(self, screen): ## this is considered a method (a regular function)
+		screen.blit(self.img, self.rect) ## places image on rectangle
+	def update(self):
+		global width, height
+		# checks the boundaries
+		if self.rect.right + self.dx >= width or self.rect.left + self.dx <= 0:
+			self.dx = self.dx * -1
+		self.rect.x += self.dx
+	def changeDirection(self):
+		self.dx *= 1
+		self.rect.y += 40
+	def checkForWallCollision(self):
+		return self.rect.right + self.dx >= width or self.rect.left + self.dx <= 0
+
+		
 class InvaderGroup:
 	def __init__(self):
 		self.invaderList = []
@@ -35,39 +53,77 @@ class InvaderGroup:
 	def draw(self,screen):
 		for i in self.invaderList:
 			i.draw(screen)
+	def update(self):
+		has_collided = False
+		for i in self.invaderList:
+			if i.checkForWallCollision():
+				has_collided = True
+		if has_collided:
+			for i in self.invaderList:
+				i.changeDirection()
+		for i in self.invaderList:
+			i.update()
 
-
-
-
-
-width = 800
-height = 600
-
-c = time.Clock()
-window = display.set_mode((width, height))
-
-endGame = False
-
-x = 30
-count = 0
-
-while count < 6:  # creates 6 invaders
-    newInvader = Invader(x, 60)
-    invaderList.append(newInvader)
-    x += 120
-    count += 1
-
+class Ship:
+	def __init__(self):
+		self.rect = Rect(400,520,40,80)
+		self.img = image.load("ship.png")
+		self.img = transform.scale(self.img, (40,80))
+		self.dx = 0
+	def update(self):
+		global width
+		if self.rect.left + self.dx < 0 or self.rect.right + self.dx > width:
+			self.stop()
+		self.rect.x += self.dx
+	def moveLeft(self):
+		self.dx = -5
+	def moveRight(self):
+		self.dx = 5
+	def stop(self):
+		self.dx = 0
+	def draw(self,screen):
+		screen.blit(self.img,self.rect)
+		
+class Bullet:
+	def __init__(self, x,y):
+		self.rect = Rect(x,y,5,10)
+	
+	def update(self):
+		self.rect.y -= 5
+	
+	def draw(self,screen):
+		draw.rect(screen, (255,255,255), self.rect)
+endGame = False ##endgame boolean
+screen.fill((255,255,255))
 ig = InvaderGroup()
-while not endGame:
-    for e in event.get():
-        if e.type == QUIT:
-            endGame = True
+player = Ship()
 
-    window.fill((0, 0, 0))
+while not endGame: ## gameloop (while the game hasn't finished)
+	for e in event.get():
+		if e.type == QUIT:
+			endGame = True
+		if e.type == KEYDOWN:
+			if e.key == K_LEFT:
+				player.moveLeft()
+			elif e.key == K_RIGHT:
+				player.moveRight()
+		if e.type == KEYUP:
+			if e.key == K_LEFT or e.key == K_RIGHT:
+				player.stop()
+					
+			
+	count = 0
+	x = 10
+	y = 20
+	
+	row = 0
+	col = 0
+		
+	screen.fill((0,0,0))
+	ig.update()
+	ig.draw(screen)
+	player.update()
+	player.draw(screen)	
 
-    for invader in invaderList:
-        invader.draw(window)
-    
-    ig.draw(window)
-    c.tick(FPS)
-    display.flip()
+	c.tick(FPS)
+	display.flip() ## shows the screen (the game)
